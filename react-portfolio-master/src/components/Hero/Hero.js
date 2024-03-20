6505737e6cd9e2e70b86a83eb5fd7ccf580abbe6
mel-muhina <mel.muhina@gmail.com>
2024-03-20 22:51:41 +0000

    Updated Hero Bug

--- react-portfolio-master/src/components/Hero/Hero.js
+++ react-portfolio-master/src/components/Hero/Hero.js
@@ -5,13 +5,13 @@ import heroImage from "../../assets/hero/ProfileMini.jpg"
 
 
 export const Hero = () => {
-    return (<section className={styles.container}>
-                    <div className={styles.content}>
-                        <h1 className={styles.title}>
+    return (<section className={styles.heroContainer}>
+                    <div className={styles.heroContent}>
+                        <h1 className={styles.herotitle}>
                             Hi, I'm Mel!
                         </h1>
-                        <p className={styles.description}> Former teacher turned frontend developer specialising in HTML, CSS, JavaScript and React. Eager to collaborate on projects that prioritise user-centric design and intuitive interfaces.  Send me a message if you would like to find out more.</p>
-                        <a className={styles.contactBtn} href="mailto:mel.muhina@gmail.com"> Contact Me </a>
+                        <p className={styles.herodescription}> Former teacher turned frontend developer specialising in HTML, CSS, JavaScript and React. Eager to collaborate on projects that prioritise user-centric design and intuitive interfaces.  Send me a message if you would like to find out more.</p>
+                        <a className={styles.herocontactBtn} href="mailto:mel.muhina@gmail.com"> Contact Me </a>
                      </div>
                      <img src={heroImage} alt="Mel Muhina" className={styles.heroImg}/>
                      <div className={styles.topBlur} />
